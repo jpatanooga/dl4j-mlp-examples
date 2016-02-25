50d8aa84874450c1d6c74728cf1b442626caac9a
Josh Patterson <jpatterson@floe.tv>
2016-02-25 09:31:31 -0500

    basic maven setup

---
+++ src/main/java/tv/floe/dl4j/examples/mlp/App.java
@@ -0,0 +1,13 @@
+package tv.floe.dl4j.examples.mlp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
